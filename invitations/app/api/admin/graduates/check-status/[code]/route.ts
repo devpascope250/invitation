4d53637cf5a682f8cd2c3f7c7bacb15214183db6
pascal <pascope2500@gmail.com>
2025-09-30 23:20:01 +0300

    modify db url

--- invitations/app/api/admin/graduates/check-status/[code]/route.ts
+++ invitations/app/api/admin/graduates/check-status/[code]/route.ts
@@ -5,52 +5,46 @@ export async function GET(request: Request, { params }: { params: Promise<{ code
   const { code } =  await params;   
   // get id from code invitationId:regNumber
   const invitationId = code.split(":")[0];
-  try{
-    
   // const regNumber = code.split(":")[1];
 
-  const graduate = await prisma.invitationCard.findFirst({
+  const graduate = await prisma.invitationCard.findUnique({
     where: {
       id: invitationId,
     },
   });
-  return NextResponse.json({ message: "Graduate found"});
-  }catch(error){
 
-    return NextResponse.json({ message: "Invitation not found", error: error }, { status: 500 });
+  if (!graduate) {
+      return NextResponse.json({ message: "Invitation not found" }, { status: 404 });
   }
-//   if (!graduate) {
-//       return NextResponse.json({ message: "Invitation not found" }, { status: 404 });
-//   }
 
-//   if(graduate.status === "SCANNED"){
-//     return NextResponse.json({ message: "Invitation already scanned" }, { status: 400 });
-//   }
+  if(graduate.status === "SCANNED"){
+    return NextResponse.json({ message: "Invitation already scanned" }, { status: 400 });
+  }
 
-// const updateDate = await prisma.invitationCard.update({
-//     where: {
-//       id: invitationId,
-//     },
-//     data: {
-//       status: "SCANNED",
-//     },
-//   });
+const updateDate = await prisma.invitationCard.update({
+    where: {
+      id: invitationId,
+    },
+    data: {
+      status: "SCANNED",
+    },
+  });
 
-// const graduateData = {
-//     id: updateDate.id,
-//     fullName: graduate.fullName,
-//     regNumber: graduate.regNumber ?? "",
-//     email: graduate.email ?? "",
-//     phoneNumber: graduate.phoneNumber ?? "",
-//     status: updateDate.status,
-//     type: updateDate.type,
-//     position: updateDate.position,
-//     origanization: updateDate.origanization,
-//     createdAt: updateDate.dateScanned
-// }
+const graduateData = {
+    id: updateDate.id,
+    fullName: graduate.fullName,
+    regNumber: graduate.regNumber ?? "",
+    email: graduate.email ?? "",
+    phoneNumber: graduate.phoneNumber ?? "",
+    status: updateDate.status,
+    type: updateDate.type,
+    position: updateDate.position,
+    origanization: updateDate.origanization,
+    createdAt: updateDate.dateScanned
+}
 
-//   return NextResponse.json({ message: "Graduate found", graduate: graduateData }, { status: 200
-//   });
+  return NextResponse.json({ message: "Graduate found", graduate: graduateData }, { status: 200
+  });
 }
 
 
